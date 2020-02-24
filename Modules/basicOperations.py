0103baab4652e6d47a348c693045438620ebcc62
Jorge Rojas <jorgearojas25@gmail.com>
2020-02-23 20:33:58 -0500

    Ejercicios de modulos

---
+++ Modules/basicOperations.py
@@ -0,0 +1,20 @@
+# Crear un módulo de funciones aritméticas que realicen las operaciones de suma, resta,
+# multiplicación, división y potencia de enteros, escribir un programa que importe este
+# módulo y use estas funciones para operar con números capturados desde el teclado.
+
+def suma(valor1, valor2):
+    return valor1+valor2
+
+def resta(valor1, valor2):
+    return valor1-valor2
+
+def mult(valor1, valor2):
+    return valor1*valor2
+
+def div(valor1, valor2):
+    return valor1/valor2
+
+def potencia(valor1, valor2):
+    return valor1**valor2
+
+
--- Modules/hexa.py
+++
@@ -1,2 +0,0 @@
-# Crear un módulo que maneje funciones para operar en sistema hexadecimal, las operaciones
-# que se deben implementar son suma, resta, multiplicación y división.
