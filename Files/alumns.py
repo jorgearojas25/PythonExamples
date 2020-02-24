da8a5669ae12448ab77db97a525ddef54fedc96e
Jorge Rojas <jorgearojas25@gmail.com>
2020-02-23 21:13:45 -0500

    Ejercicios files

--- Files/alumns.py
+++
@@ -1,3 +0,0 @@
-# Escriba un programa maneje una lista de alumnos en un archivo se debe permitir agregar
-# alumnos a la lista, modificar la información de un alumno y eliminar alumnos de esta lista,
-# la información que se debe manejar es código, nombre, apellido, carrera y estado.
